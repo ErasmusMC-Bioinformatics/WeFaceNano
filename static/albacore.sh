6f40d86eaa8228f30f271ac4875faa3fb03f5e60
rjansen1984 <rick.jansen1984@gmail.com>
2019-03-12 16:53:25 +0100

    Removed unused code

--- static/albacore.sh
+++ static/albacore.sh
@@ -7,47 +7,30 @@ SPECIES=0
 
 while getopts bmrsh:-c:-i:-o:-f: option
 do
-#key="$1"
 case "${option}" in
     b|barcoding)
     BARCODING=1
-#     shift # past argument
-#     shift # past value
     ;;
     c|config)
     CONFIG=${OPTARG}
-#     shift # past argument
-#     shift # past value
     ;;
     i|input)
     FAST5Files=${OPTARG}
-#     shift # past argument
-#     shift # past value
     ;;
     o|output)
     FILEOUT=${OPTARG}
-#     shift # past argument
-#     shift # past value
     ;;
     f|format)
     FORMAT=${OPTARG}
-#     shift # past argument
-#     shift # past value
     ;;
     m|mlst)
     MLST=1
-#     shift # past argument
-#     shift # past value
     ;;
     r|res)
     RES=1
-#     shift # past argument
-#     shift # past value
     ;;
     s|species)
     SPECIES=1
-#     shift # past argument
-#     shift # past value
     ;;
     h|help) 
     echo "bash npanalysis (-b -m -s -r) -c -i -o -f"
@@ -118,25 +101,3 @@ if [ "${BARCODING}" = 0 ]; then
       FINISHED=${FILEOUT}/finished.dat
       touch ${FINISHED}
 fi
-# speciesTyping
-#if [ "${SPECIES}" = 1 ]; then
-#      gnome-terminal -e 'bash -c "jsa.util.streamServer -port 3456 | bwa mem -t 10 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -Y -K 10000 ~/japsa/SpeciesTyping/Bacteria/genomeDB.fasta  - 2> /dev/null | jsa.np.rtSpeciesTyping -bam - -index ~/japsa/SpeciesTyping/Bacteria/speciesIndex --read 5 -time 10 -out '"${FILEOUT}"'/species/speciesTypingResults.out"' &
-#      sleep 5
-#      gnome-terminal -e 'bash -c "jsa.np.npreader -realtime -streams 0.0.0.0:3456 -folder '"${FILEOUT}"'/workspace/pass --output - -format fastq --exhaustive"'
-#fi
-# MLST
-#if [ "${MLST}" = 1 ]; then
-#      gnome-terminal -e 'bash -c "jsa.util.streamServer --port 3457 | bwa mem -t 8 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -a -Y ~/japsa/MLST/Klebsiella_pneumoniae/bwaIndex/genes.fasta - | jsa.np.rtMLST -bam - -mlst ~/japsa/MLST/Klebsiella_pneumoniae/ -read 5 -time 10  --out '"${FILEOUT}"'/MLST.dat"' &
-#      sleep 5
-#      gnome-terminal -e 'bash -c "jsa.np.npreader -realtime -streams 0.0.0.0:3457 -folder '"${FILEOUT}"'/workspace/pass --output - -format fastq --exhaustive"'
-#fi
-# resFinder
-#if [ "${RES}" = 1 ]; then
-#      gnome-terminal -e 'bash -c "jsa.util.streamServer --port 3458 | bwa mem -t 2 -k11 -W20 -r10 -A1 -B1 -O1 -E1 -L0 -Y ~/japsa/ResGene/resFinder/DB.fasta - 2> /dev/null | jsa.np.rtResistGenes -bam - -score=0.00001 -read 5 -time 10 -resDB ~/japsa/ResGene/resFinder -tmp /tmp/_tmp_ -o '"${FILEOUT}"'/resfinder/resFinder.out"' &
-#      sleep 60
-#      gnome-terminal -e 'bash -c "jsa.np.npreader -realtime -streams 0.0.0.0:3458 -folder '"${FILEOUT}"'/workspace/pass --output - -format fastq --exhaustive"'
-#      gnome-terminal -e 'bash -c "jsa.np.npreader --realtime --folder='"${FILEOUT}"'/workspace/pass --output=- --exhaustive | bwa mem -t12 -x ont2d -K10000 /home/myfair/japsa/ResGene/resFinder/DB.fasta - 2> /dev/null | jsa.np.rtResistGenes --output='"${FILEOUT}"'/resfinder/resfinder.out --bamFile=- --resDB=/home/myfair/japsa/ResGene/resFinder --time=60 --tmp=tmp/resTest 2> '"${FILEOUT}"'/res.log"'
-#fi
-
-# npReader
-#gnome-terminal -e 'bash -c "jsa.np.npreader -realtime -streams 0.0.0.0:3456,0.0.0.0:3457,0.0.0.0:3458 -folder '"${FILEOUT}"'/workspace/pass --output output.fastq -format fastq --exhaustive"'
