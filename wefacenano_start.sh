9abf16f573f158ff8f639eb60bc5ca98d0d644cf
Saskia Hiltemann <zazkia@gmail.com>
2020-09-14 13:30:55 +0200

    do not assume installation is in home dir

--- wefacenano_start.sh
+++ wefacenano_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# wait just to be safe
-sleep 5
-cd $HOME/WeFaceNano
-python3 $HOME/WeFaceNano/manage.py makemigrations
-python3 $HOME/WeFaceNano/manage.py migrate --noinput
-python3 $HOME/WeFaceNano/manage.py runserver 0.0.0.0:8008
+python3 makemigrations
+python3 manage.py migrate --noinput
+python3 manage.py runserver 0.0.0.0:8008
