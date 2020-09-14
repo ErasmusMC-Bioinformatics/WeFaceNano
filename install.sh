b4de5e1f99cd5adee5c41fde682615f0b2555351
Saskia Hiltemann <zazkia@gmail.com>
2020-09-14 21:40:03 +0200

    install into nanopore-drive

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
-cd $HOME
+#!/bin/bash
+
+# install simple-circularise
 git clone https://github.com/Kzra/Simple-Circularise
-git clone https://git@bitbucket.org/genomicepidemiology/resfinder_db.git
-git clone https://bitbucket.org/genomicepidemiology/plasmidfinder_db
+
+# install kmergenie
 wget http://kmergenie.bx.psu.edu/kmergenie-1.7051.tar.gz
-tar -xvzf kmergenie-1.7051.tar.gz
-cd $HOME/kmergenie-1.7051 && make
-rm -rf $HOME/kmergenie-1.7051.tar.gz*
-cd $HOME
+tar xvzf kmergenie-1.7051.tar.gz
+cd kmergenie-1.7051 && make
+
+# cleanup
+rm -rf ../kmergenie-1.7051.tar.gz*
+cd ../
+
+# get reference databases
+mkdir -p nanopore-drive/blastdb
+cd nanopore-drive
+git clone https://git@bitbucket.org/genomicepidemiology/resfinder_db
+git clone https://bitbucket.org/genomicepidemiology/plasmidfinder_db
+
+
