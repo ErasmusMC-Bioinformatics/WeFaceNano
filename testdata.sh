456fbfff0d940a33e49d066fdba82acf3a6d46d2
Rick Jansen <rick.jansen1984@gmail.com>
2021-04-14 13:45:20 +0200

    Moved samples to separate folders

--- testdata.sh
+++ testdata.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 mkdir -p nanopore-drive/admin/testrun/fasta/
-mkdir -p nanopore-drive/admin/testrun-small/fasta/
+mkdir -p nanopore-drive/admin/testrun-small/RB01/
+mkdir -p nanopore-drive/admin/testrun-small/RB02/
 cd nanopore-drive/admin/testrun/fasta/
 
 wget ftp://parrot.genomics.cn/gigadb/pub/10.5524/100001_101000/100387/MinION_reads-12_samples.rar
 unrar e MinION_reads-12_samples.rar .
 
 # create a folde with a subset of the data for speed
-cp RB01.fasta ../../testrun-small/fasta/
-cp RB02.fasta ../../testrun-small/fasta/
+cp RB01.fasta ../../testrun-small/RB01/
+cp RB02.fasta ../../testrun-small/RB02/
